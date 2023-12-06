92044faf8ea199620738b7150fc30411809b2de3
Felix Roberge <roberge.felix@gmail.com>
2023-12-06 16:35:00 -0500

    integrate jira

--- packages/app/src/components/catalog/EntityPage/index.tsx
+++ packages/app/src/components/catalog/EntityPage/index.tsx
@@ -1,4 +1,6 @@
 import { EntitySwitch, isKind } from '@backstage/plugin-catalog';
+import { EntityJiraOverviewCard, isJiraAvailable } from '@roadiehq/backstage-plugin-jira';
+
 import React from 'react';
 
 import {
@@ -11,6 +13,7 @@ import {
   domainPage,
   resourcePage,
 } from './Pages';
+import { Grid } from '@mui/material';
 
 export const entityPage = (
   <EntitySwitch>
@@ -21,7 +24,11 @@ export const entityPage = (
     <EntitySwitch.Case if={isKind('system')} children={systemPage} />
     <EntitySwitch.Case if={isKind('domain')} children={domainPage} />
     <EntitySwitch.Case if={isKind('resource')} children={resourcePage} />
-
+    <EntitySwitch.Case if={isJiraAvailable}>
+        <Grid item md={6}>
+          <EntityJiraOverviewCard />
+        </Grid>
+        </EntitySwitch.Case>
     <EntitySwitch.Case>{defaultEntityPage}</EntitySwitch.Case>
   </EntitySwitch>
 );
