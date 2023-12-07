5f72bc80925403c56ee3268018fbafa521649ab0
masauve <msauve@redhat.com>
2023-12-07 12:33:47 -0500

    dummy user

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -76,7 +76,7 @@ export default async function createPlugin(
       github: providers.github.create({
         signIn: {
           resolver(_, ctx) {
-            const userRef = 'user:default/guest'; // Must be a full entity reference
+            const userRef = 'user:default/masauve'; // Must be a full entity reference
             return ctx.issueToken({
               claims: {
                 sub: userRef, // The user's own identity
